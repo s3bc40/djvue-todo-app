cf218fe1e9d188c9e2eebd2d3bf014a8d0202c50
s3bc40 <s.goncalvesclaro@gmail.com>
2020-11-21 15:59:18 +0100

    Serialize tag into task Vue js communication done

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 from .models import Task, Tag
 
 
-class TaskSerializer(serializers.ModelSerializer):
-    """Allow to serialize our Task model for the API"""
-
-    class Meta:
-        model = Task
-        fields = '__all__'
-
-
 class TagSerializer(serializers.ModelSerializer):
     """Allow to serialize our Tag model for the API"""
     class Meta:
         model = Tag
         fields = '__all__'
     
+
+class TaskSerializer(serializers.ModelSerializer):
+    """Allow to serialize our Task model for the API"""
+    tags = TagSerializer(read_only=True, many=True)
+    
+    class Meta:
+        model = Task
+        fields = '__all__'
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   mounted () {
     axios.get('http://localhost:8000/api/tasks')
